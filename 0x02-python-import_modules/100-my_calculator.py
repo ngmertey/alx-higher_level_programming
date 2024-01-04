5e4b22e3ee3dd8be4d4bc9dbb1a74c1874f26187
Joshua <ngmerteyjoshua360@gmail.com>
2024-01-04 15:14:51 -0800

    a program that imports all functions from the file calculator_1.py and handles basic operations.

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+
     from calculator_1 import add, sub, mul, div
-    import system
+    import sys
 
     if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    optrs = {"+": add, "-": sub, "*": mul, "/": div}
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
     if sys.argv[2] not in list(ops.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
