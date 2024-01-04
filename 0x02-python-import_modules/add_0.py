9ba28648741eb46ba6aab1f4854423d20b9b839b
Joshua <ngmerteyjoshua360@gmail.com>
2024-01-04 11:16:15 -0800

    a file containing and addition function

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def add(a, b):
+    """Addition function
+
+    Args:
+        a: first integer
+        b: second integer
+        sum: addition of a and b
+
+    Returns:
+        The return value sum
+    """
+    sum = a + b
+    return sum
